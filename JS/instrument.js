cb7b936ef747be387d9362be5f724a4ded606e95
wadeinator74 <wadecasper6@gmail.com>
2018-09-18 13:32:20 -0600

    Added main website
    Made this website in 8th grade

---
+++ JS/instrument.js
@@ -0,0 +1,76 @@
+// JavaScript Document
+
+$(document).ready(function(e) {
+    //this next section will select the c box and add 
+	//a play function to the box
+		var c_note = document.getElementById('cNote');
+	$('#c').mousedown(function(){
+		c_note.currentTime = 0;
+		c_note.play();
+		});
+		var c_note = document.getElementById('cNote');
+	$('#c').mouseenter(function(){
+		c_note.currentTime = 0;
+		c_note.play();
+		});
+		var d_note = document.getElementById('dNote');
+	$('#d').mousedown(function(){
+		d_note.currentTime = 0;
+		d_note.play();
+		});
+		var d_note = document.getElementById('dNote');
+	$('#d').mouseenter(function(){
+		d_note.currentTime = 0;
+		d_note.play();
+		});
+		var e_note = document.getElementById('eNote');
+	$('#e').mousedown(function(){
+		e_note.currentTime = 0;
+		e_note.play();
+		});
+		var e_note = document.getElementById('eNote');
+	$('#e').mouseenter(function(){
+		e_note.currentTime = 0;
+		e_note.play();
+		});
+		var f_note = document.getElementById('fNote');
+	$('#f').mousedown(function(){
+		f_note.currentTime = 0;
+		f_note.play();
+		});
+		var f_note = document.getElementById('fNote');
+	$('#f').mouseenter(function(){
+		f_note.currentTime = 0;
+		f_note.play();
+		});
+		var g_note = document.getElementById('gNote');
+	$('#g').mousedown(function(){
+		g_note.currentTime = 0;
+		g_note.play();
+		});
+		var g_note = document.getElementById('gNote');
+	$('#g').mouseenter(function(){
+		g_note.currentTime = 0;
+		g_note.play();
+		});
+		var a_note = document.getElementById('aNote');
+	$('#a').mousedown(function(){
+		a_note.currentTime = 0;
+		a_note.play();
+		});
+		var a_note = document.getElementById('aNote');
+	$('#a').mouseenter(function(){
+		a_note.currentTime = 0;
+		a_note.play();
+		});
+		var b_note = document.getElementById('bNote');
+	$('#b').mousedown(function(){
+		b_note.currentTime = 0;
+		b_note.play();
+		});
+		var b_note = document.getElementById('bNote');
+	$('#b').mouseenter(function(){
+		b_note.currentTime = 0;
+		b_note.play();
+		});
+});
